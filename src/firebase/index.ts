e84c16c24e9d175208f68f50c8b10ec724f3118b
ceivotoivo <153224234+ceivotoivo@users.noreply.github.com>
2023-12-09 00:31:55 +0200

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_PROJECT_ID + ".firebaseapp.com",
   databaseURL:
-    "https://" + import.meta.env.VITE_FIREBASE_PROJECT_ID + ".firebaseio.com",
+    "https://zaruba-4f5de-default-rtdb.firebaseio.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_PROJECT_ID + ".appspot.com",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
